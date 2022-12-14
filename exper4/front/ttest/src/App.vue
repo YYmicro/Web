bebe231275d31bf4021ca447186e82b342f399ee
YYmicro <YYmicro1044566658@outlook.com>
2022-12-14 21:37:03 +0800

    :recycle: re-write the heading

--- exper4/front/ttest/src/App.vue
+++ exper4/front/ttest/src/App.vue
@@ -1,152 +1,62 @@
-<script lang="ts" setup>
-import { ref, reactive, onMounted } from "vue";
-// import axios from "axios";
-// import { keyBy } from "lodash-es";
-// axios.defaults.withCredentials = true;
-import MyMenu from './components/App/menu.vue'
-const mymenu = ref(MyMenu)
-import MyNews from './components/App/RecentNews.vue'
-const mynews = ref(MyNews)
-import MyPapers from './components/App/Papers.vue'
-const mypapers = ref(MyPapers)
-import ResearchDirect from './components/App/ResearchDirect.vue'
-const researchdirect = ref(ResearchDirect)
-
-const Carousel_img_fit = 'scale-down'
+<script setup>
+import { ref, reactive, onMounted, computed } from "vue";
+import Index from './Index.vue'
+import researchIntro from './researchIntro.vue'
+const props = defineProps(['defA','actColor']);
+const defA = ref(props.defA);
+const active_color = ref("#409EFF");
+active_color.value = props.actColor;
+// console.log(props.defA)
 
 const handleSelect = () => {
 
 }
 onMounted(() => {
-  let script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js';
-  document.body.appendChild(script);
+
+})
+// 导航栏组合式
+const routes = {
+  '/': Index,
+  '/researchintro': researchIntro
+}
+// 在上面添加新的页面
+const currentPath = ref(window.location.hash)
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
 })
 </script>
 
-
 <template>
-<div class="common-layout">
-  <el-container>
-    <!-- 导航栏 -->
-    <el-header  justigy="space-evenly">
-      <el-col :span="24">
-        <mymenu defA="1"/>
-      </el-col>
-    </el-header>
-    <!-- 幻灯片 -->
-    <el-main>
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
-          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
-          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="card mb-3 w-100">
-              <div class="row g-0">
-                <div class="col-md-1 bg-dark"></div>
-                <div class="col-md-7">
-                  <img src="./assets/App/HDACP.png" class="d-block w-100" alt="...">
-                </div>
-                <div class="col-md-3">
-                  <div class="card-body">
-                    <h5 class="card-title"><b>实验室介绍</b></h5>
-                    <hr/>
-                    <p class="card-text">&nbsp;&nbsp;&nbsp;&nbsp;青海大学高性能与云计算研究所HDACP实验室(High-performance computing and Distributed systems, Computer vision and Pattern recognition)成立于2020年， 是青海大学高性能与云计算研究所开展国内外广泛交流的高性能计算与分布式系统以及计算机视觉研究的科研小组。 HDACP实验室的目标是：依托研究所，建立起具有国内外知名水平的研究组。</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-                <div class="col-md-1 bg-dark"></div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="card mb-3 w-100">
-              <div class="row g-0">
-                <div class="col-md-1 bg-dark"></div>
-                <div class="col-md-7">
-                  <img src="./assets/App/2.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="col-md-3">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-                <div class="col-md-1 bg-dark"></div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="card mb-3 w-100">
-              <div class="row g-0">
-                <div class="col-md-1 bg-dark"></div>
-                <div class="col-md-7">
-                  <img src="./assets/App/3.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="col-md-3">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-                <div class="col-md-1 bg-dark"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </a>
-      </div>
-      <br/><br/><br/><br/>
-      <el-row>
-        <el-col :span="1"></el-col>
-        <el-col :span="6">
-          <mynews />
-        </el-col>
-        <el-col :span="10">
-          <mypapers />
-        </el-col>
-        <el-col :span="6">
-          <researchdirect />
-        </el-col>
-        <el-col :span="1"></el-col>
-      </el-row>
-    </el-main>
-    <el-footer>Footer</el-footer>
-  </el-container>
-</div>
+  <el-menu :default-active="defA" :active-text-color="active_color" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1" class="home"><a href="#/"><p class="home_font all_font">HDACP</p></a></el-menu-item>
+    <el-menu-item index="2"><a href="#/researchintro"><p class="all_font">研究介绍</p></a></el-menu-item>
+    <el-menu-item index="3"><p class="all_font">科研团队</p></el-menu-item>
+    <el-menu-item index="4"><p class="all_font">新闻动态</p></el-menu-item>
+    <el-menu-item index="5"><p class="all_font">科研项目</p></el-menu-item>
+    <el-menu-item index="6"><p class="all_font">联系我们</p></el-menu-item>
+  </el-menu>
+  <component :is="currentView" />
 </template>
 
 
 <style>
-@import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css";
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+.all_font{
+  font-weight:bold;
+  margin: 2px;
+  font-size: 16px;
 }
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+.home{
+  background-color: rgb(0, 183, 195);
+  /* background-color: #00ffff; */
 }
-.el_image{
-  width: 400px;
-  height: 350px;
+.home_font{
+  color: white;
+  font-size:18px;
 }
-.el-header{
-  padding:0px !important;
-}
-.el-main{
-  padding:0px !important;
-}
+a{text-decoration: none;}
 </style>
+
---
+++ exper4/front/ttest/src/components/Index/RecentNews.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { ref, reactive, onMounted } from "vue";
+
+const props = defineProps([]);
+// console.log(props.defA)
+
+const news_titles = ['祝贺: 实验室王晓英教授遴选为青海大学博士生导师',
+                    '祝贺: 实验室黄建强教授遴选为青海大学博士生导师',
+                    '祝贺: 实验室朱宇博士遴选为青海大学硕士生导师',
+                    '第二十四届IEEE International Conference on High Performance Computing and Communications (HPCC-2022)正在征文',
+]
+
+
+const handleSelect = () => {
+
+}
+onMounted(() => {
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js';
+  document.body.appendChild(script);
+})
+</script>
+
+<template>
+    <div class="card w-100 p-2" style="border: none;">
+        <img src="../../assets/Index/1.jpg" class="card-img-top w-100">
+        <div class="card-body mybg">
+            <h5 class="card-title"><b>最新动态</b></h5>
+            <el-card class="news_card" shadow="hover" v-for="news_title in news_titles" :key="news_title">{{news_title}}</el-card>
+        </div>
+    </div>
+</template>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css";
+
+.mybg{
+    background-color: rgba(0, 0, 0, 0.15);
+}
+
+.news_card{
+    font-weight: bold;
+    color: rgb(0, 156, 189);
+    margin: 2px;
+}
+
+</style>
+
