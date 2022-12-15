108dd4417003e6fd48c0abe35ae55b0fa5663288
YYmicro <YYmicro1044566658@outlook.com>
2022-12-15 17:48:59 +0800

    research info

--- exper4/front/ttest/src/components/researchIntro/HPC.vue
+++ exper4/front/ttest/src/components/researchIntro/HPC.vue
@@ -1,35 +1,41 @@
 <template>
-    <h1>方向介绍</h1>
-    <p>高性能计算（High performance computing）指通常使用很多处理器（作为单个机器的一部分）或者某一集群中组织的几台计算机（作为单个计算资源操作）的计算系统和环境。高性能集群上运行的应用程序一般使用并行算法，把一个大的普通问题根据一定的规则分为许多小的子问题，在集群内的不同节点上进行计算，而这些小问题的处理结果，经过处理可合并为原问题的最终结果。由于这些小问题的计算一般是可以并行完成的，从而可以缩短问题的处理时间。
-
-Coast
-高性能集群在计算过程中，各节点是协同工作的，它们分别处理大问题的一部分，并在处理中根据需要进行数据交换，各节点的处理结果都是最终结果的一部分。高性能集群的处理能力与集群的规模成正比，是集群内各节点处理能力之和，但这种集群一般没有高可用性。高性能计算的分类方法很多。这里从并行任务间的关系角度来对高性能计算分类。
-
-高吞吐计算（High-throughput Computing）是高性能计算的类别之一，可以把它分成若干可以并行的子任务，而且各个子任务彼此间没有什么关联。因为这种类型应用的一个共同特征是在海量数据上搜索某些特定模式，所以把这类计算称为高吞吐计算。所谓的Internet计算都属于这一类。按照Flynn的分类，高吞吐计算属于SIMD （Single Instruction Multiple Data ，单指令多数据流）的范畴。
-
-分布计算（Distributed Computing）刚好和高吞吐计算相反，它们虽然可以分成若干并行的子任务，但是子任务间联系紧密，需要大量的数据交换。按照Flynn的分类，分布式的高性能计算属于MIMD（Multiple Instruction Multiple Data，多指令流-多数据流）的范畴。
-
-高性能计算机典型的应用领域有：
-1. 气象预报：借助超级计算机能够模拟复杂的气流、洋流变化，能够预测天气走向。
-2. 计算化学应用：传统的计算化学主要是研究原子和分子尺度的小分子体系的行为和相互作用。在这个领域里使用的计算理论主要包括Hartree-Fock方法（HF）、密度泛函理论（DFT）、多体扰动理论（MBPT）、组态相互作用理论（CI）和藕合簇理论（CC）等。高性能计算机能很好解决这些计算的准确性。
-3. 发现生命秘密：从基因学的数据密集型研究到细胞网络模拟，超级计算机将帮助寻找生命的秘密。
-4. 流体力学、地质模拟等大型科学计算领域、以及人工智能的一些领域。</p>
-<!-- 字符串 -->
-<router-link to="/home">Home</router-link>
-<!-- 渲染结果 -->
-<a href="/home">Home</a>
-
-<!-- 使用 v-bind 的 JS 表达式 -->
-<router-link :to="'/home'">Home</router-link>
-
-<!-- 同上 -->
-<router-link :to="{ path: '/home' }">Home</router-link>
-
-<!-- 命名的路由 -->
-<router-link :to="{ name: 'user', params: { userId: '123' }}">User</router-link>
-
-<!-- 带查询参数，下面的结果为 `/register?plan=private` -->
-<router-link :to="{ path: '/register', query: { plan: 'private' }}">
-  Register
-</router-link>
+    <!-- <h1>方向介绍</h1> -->
+    <div class="tz-gallery">
+					
+                    <b>方向介绍：</b>		
+                    
+                    <p style="font-size:0.9rem;">
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;高性能计算（High performance computing）指通常使用很多处理器（作为单个机器的一部分）或者某一集群中组织的几台计算机（作为单个计算资源操作）的计算系统和环境。高性能集群上运行的应用程序一般使用并行算法，把一个大的普通问题根据一定的规则分为许多小的子问题，在集群内的不同节点上进行计算，而这些小问题的处理结果，经过处理可合并为原问题的最终结果。由于这些小问题的计算一般是可以并行完成的，从而可以缩短问题的处理时间。
+                                    <br>
+                                    <br>
+                                    
+                                    </p><div align="center">
+                                        <a class="lightbox">
+                                            <img class="research-sm-img" height="400" width="400" src="../../assets/researchIntro/chatu_gxn.jpg" alt="Coast">
+                                        </a>
+                                    </div>
+                                    
+                                    &nbsp;&nbsp;&nbsp;&nbsp;高性能集群在计算过程中，各节点是协同工作的，它们分别处理大问题的一部分，并在处理中根据需要进行数据交换，各节点的处理结果都是最终结果的一部分。高性能集群的处理能力与集群的规模成正比，是集群内各节点处理能力之和，但这种集群一般没有高可用性。高性能计算的分类方法很多。这里从并行任务间的关系角度来对高性能计算分类。
+                                    <br>
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;高吞吐计算（High-throughput Computing）是高性能计算的类别之一，可以把它分成若干可以并行的子任务，而且各个子任务彼此间没有什么关联。因为这种类型应用的一个共同特征是在海量数据上搜索某些特定模式，所以把这类计算称为高吞吐计算。所谓的Internet计算都属于这一类。按照Flynn的分类，高吞吐计算属于SIMD （Single Instruction Multiple Data ，单指令多数据流）的范畴。
+                                    <br>
+                                    <br>
+                                    分布计算（Distributed Computing）刚好和高吞吐计算相反，它们虽然可以分成若干并行的子任务，但是子任务间联系紧密，需要大量的数据交换。按照Flynn的分类，分布式的高性能计算属于MIMD（Multiple Instruction Multiple Data，多指令流-多数据流）的范畴。
+                                    <br>
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;高性能计算机典型的应用领域有：
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;1. 气象预报：借助超级计算机能够模拟复杂的气流、洋流变化，能够预测天气走向。
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;2. 计算化学应用：传统的计算化学主要是研究原子和分子尺度的小分子体系的行为和相互作用。在这个领域里使用的计算理论主要包括Hartree-Fock方法（HF）、密度泛函理论（DFT）、多体扰动理论（MBPT）、组态相互作用理论（CI）和藕合簇理论（CC）等。高性能计算机能很好解决这些计算的准确性。
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;3. 发现生命秘密：从基因学的数据密集型研究到细胞网络模拟，超级计算机将帮助寻找生命的秘密。
+                                    <br>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;4. 流体力学、地质模拟等大型科学计算领域、以及人工智能的一些领域。
+                    
+                    <p></p>
+                    
+                                    </div>
 </template>
--- exper4/front/ttest/src/researchIntro.vue
+++ exper4/front/ttest/src/researchIntro.vue
@@ -2,71 +2,58 @@
 import { onMounted, ref, computed } from "vue"
 import HPC from './components/researchIntro/HPC.vue'
 import GCS from './components/researchIntro/GraphicsComputingSystem.vue'
+import dl from './components/researchIntro/DeepLearning.vue'
+import CV from './components/researchIntro/ComputerVision.vue'
+import GC from './components/researchIntro/GreenComputing.vue'
 import NotFound from './NotFound.vue'
 
 const hpc = ref(HPC);
 const gcs = ref(GCS);
+const DL = ref(dl);
+const cv = ref(CV);
+const gc = ref(GC);
 const notfound = ref(NotFound);
 const now = ref(HPC);
 
-function getItem(item) {
-  if(item == 'hpc'){
-    console.log('hpc');
-    now.value = hpc.value;
-  }else if(item == 'gcs'){
-    console.log('gcs');
-    now.value = gcs.value;
-  }else{
-    now = notfound;
-  }
-}
-
+const articles = ref(["高性能计算","图计算系统","深度学习","计算机视觉","绿色计算"]);
 onMounted(() => {
   let script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js';
   document.body.appendChild(script);
 })
-// // 导航栏组合式
-// const researches = {
-//   '/researchintro': HPC,
-//   '/researchintro/gcs': GCS
-//   // '/researchintro': researchIntro,
-// }
-// // 在上面添加新的页面
-// const researchPath = ref(window.location.hash)
-// window.addEventListener('hashchange', () => {
-//   researchPath.value = window.location.hash
-// })
-// const researchView = computed(() => {
-//   console.log("researchintro\n")
-//   // console.log(researchPath.value)
-//   // console.log(researchPath.value.slice(1))
-//   // console.log(researchPath.value.slice(1) || '/')
-//   console.log(researches[researchPath.value.slice(1) || '/'])
-//   return researches[researchPath.value.slice(1) || '/'] || NotFound
-// })
+
 </script>
+
 <template>
-    <div class="common-layout">
-      <el-container>
-        <el-container style="height:100%;">
-        <el-aside class="sidebar" width="200px" style="background-color:azure;height:100%">
-            <el-menu default-active="1">
-                  <!-- <el-menu-item index="1"><a href="#/researchintro">高性能计算</a></el-menu-item>
-                  <el-menu-item index="2"><a href="#/researchintro/gcs">图计算系统</a></el-menu-item> -->
-                  <el-menu-item index="1" @click="getItem('hpc')">高性能计算</el-menu-item>
-                  <el-menu-item index="2" @click="getItem('gcs')">图计算系统</el-menu-item>
-                  <el-menu-item index="3">深度学习</el-menu-item>
-                  <el-menu-item index="4">计算机视觉</el-menu-item>
-                  <el-menu-item index="5">绿色计算</el-menu-item>
-            </el-menu>
-        </el-aside>
-        <el-main>
-          <now />
-          <!-- <component :is="researchView" /> -->
-        </el-main>
-      </el-container>
-      </el-container>
-    </div>
+    <el-tabs tab-position="left" type="border-card">
+    <el-tab-pane v-for="article in articles">
+      <template #label>
+        <span class="custom-tabs-label">
+          <!-- <el-icon><calendar /></el-icon> -->
+          <span style="margin:50px;">{{article}}</span>
+        </span>
+      </template>
+      <div class="articles" v-if="article=='高性能计算'">
+      <hpc/>
+      </div>
+      <div class="articles" v-else-if="article=='图计算系统'">
+      <gcs/>
+      </div>
+      <div class="articles" v-else-if="article=='深度学习'">
+      <DL/>
+      </div>
+      <div class="articles" v-else-if="article=='计算机视觉'">
+      <cv/>
+      </div>
+      <div class="articles" v-else="article=='绿色计算'">
+      <gc/>
+      </div>
+    </el-tab-pane>
+  </el-tabs>
 </template>
+<style>
+.articles{
+  padding:30px;
+}
+</style>
